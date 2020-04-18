f71e360fa82bd7652d0506c331a0082ed37aeec1
rcrdz <63907823+rcrdz@users.noreply.github.com>
2020-04-18 17:38:43 +0200

    First R-Script

---
+++ RStudioUIinDatabricksTest.R
@@ -0,0 +1,26 @@
+SparkR::sparkR.session()
+library(sparklyr)
+sc <- spark_connect(method = "databricks")
+
+install.packages(c("nycflights13", "Lahman"))
+library(dplyr)
+iris_tbl <- copy_to(sc, iris)
+flights_tbl <- copy_to(sc, nycflights13::flights, "flights")
+batting_tbl <- copy_to(sc, Lahman::Batting, "batting")
+src_tbls(sc)
+
+# filter by departure delay and print the first few records
+flights_tbl %>% filter(dep_delay == 2)
+
+delay <- flights_tbl %>%
+  group_by(tailnum) %>%
+  summarise(count = n(), dist = mean(distance), delay = mean(arr_delay)) %>%
+  filter(count > 20, dist < 2000, !is.na(delay)) %>%
+  collect
+
+# plot delays
+library(ggplot2)
+ggplot(delay, aes(dist, delay)) +
+  geom_point(aes(size = count), alpha = 1/2) +
+  geom_smooth() +
+  scale_size_area(max_size = 2)
